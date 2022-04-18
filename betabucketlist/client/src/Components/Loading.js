276b1e1dd999a8608b8ae7db0ddbf3c0d4835e3e
Menaka Iyer <menaka@passiondaisy.com>
2022-04-18 12:24:07 -0400

    users legoo

---
+++ betabucketlist/client/src/Components/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Loading = () => {
+  return <div>Loading</div>;
+};
+
+export default Loading;
+
+
+// GIFLENS-https://media1.giphy.com/media/L4gSdAfVqhyAs9PNxq/200.gif
