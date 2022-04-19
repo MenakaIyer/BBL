13adc010298c80922ad06438018efda307f48c40
Menaka Iyer <menaka@passiondaisy.com>
2022-04-19 11:03:09 -0400

    Cloundinary upload baby

--- betabucketlist/Server/src/config.js
+++ betabucketlist/Server/src/config.js
@@ -6,3 +6,10 @@ module.exports = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
+
+
+cloudinary.config({ 
+  cloud_name: 'compassiondaisy', 
+  api_key: '513766762624921', 
+  api_secret: 'PcJZifdaipjT5pQIckTKEkedfp8' 
+});
--- betabucketlist/client/src/Components/ImageUpload.js
+++ betabucketlist/client/src/Components/ImageUpload.js
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 
 const ImageUpload = () => {
-    const [loading, setLoading] = useState(false)
-    const [image, setImage] = useState (" ")
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState(" ");
 
   const uploadImage = async (e) => {
     const files = e.target.files;
     const data = new FormData();
-    data.append(`file`, files[0])
-    data.append(`upload_preset`,`BBLImages`)
-    setLoading(true)
+    data.append(`file`, files[0]);
+    data.append(`upload_preset`, `BBLImages`);
+    setLoading(true);
 
-    const res = await fetch("")
-
+    const res = await fetch(
+      "http://api.cloudinary.com/v1_1/compassiondaisy/image/upload",
+      {
+        method: `POST`,
+        body: data,
+      }
+    );
+    const file = await res.json();
+    console.log(file);
+    setImage(file.secure_url);
+    setLoading(false);
   };
 
   return (
     <div>
       <h1 className="upload">Upload image</h1>
       <input type="file" placeholder="Upload" onChange={uploadImage} />
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <img src={image} style={{ width: `300px` }} alt={`uploaded`} />
+      )}
     </div>
   );
 };
